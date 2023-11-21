8f9f66c4f23432a5700500d7398aca4beaeba439
Dominik Tajak <domi.tajak@gmail.com>
2023-11-21 16:42:03 +0100

    last one

--- Proposal.sol
+++ Proposal.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.21;
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
-// 0x105a1E605d5D34FB096c6f35Ceb34f66e64c7710
 contract ProposalContract {
   mapping(address => mapping(uint256 => bool)) public exerciceProgression;
     struct Proposal {
@@ -20,7 +20,8 @@ contract ProposalContract {
         uint256 deadline;
         string description;
         address createdBy;
-        uint256 voterCount;        
+        uint256 voterCount;
+        bool senderVoted;        
     }
 
     Proposal[] public proposals;
@@ -37,9 +38,8 @@ contract ProposalContract {
         newProposal.createdDate = block.timestamp;
         newProposal.deadline = _deadline;
         newProposal.description = _description;
-        newProposal.createdBy = msg.sender;
-        newProposal.hasVoted[msg.sender] = true;
-        newProposal.voterCount = 1;
+        newProposal.createdBy = msg.sender;        
+        newProposal.voterCount = 0;
     }
 
     function vote(bytes32 proposalId) public {
@@ -63,16 +63,33 @@ contract ProposalContract {
     function getProposal(uint256 proposalIndex) public view returns (ProposalDto memory) {
         require(proposalIndex < proposals.length, "Invalid proposal index");
         Proposal storage proposal = proposals[proposalIndex];
+        bool senderVoted = proposal.hasVoted[msg.sender];
         ProposalDto memory proposalDto = ProposalDto(
           proposal.id,
           proposal.createdDate,
           proposal.deadline,
           proposal.description,
           proposal.createdBy,
-          proposal.voterCount
+          proposal.voterCount,
+          senderVoted
         );
         return proposalDto;
     }
+
+    function getProposalById(bytes32 proposalId) public {
+        require(proposals.length > 0, "No Proposals exist");
+        require(proposalsContainId(proposalId), "This Proposal does not exist");
+        for (uint256 i = 0; i < proposals.length; i++) {
+            if (proposals[i].id == proposalId) {
+                Proposal storage proposal = proposals[i];
+                require(block.timestamp < proposal.deadline, "Voting is closed");
+                require(!proposal.hasVoted[msg.sender], "You have already voted for this proposal");
+                proposal.hasVoted[msg.sender] = true;
+                proposal.voterCount++;
+            }
+        }  
+    }
+
 
     function proposalsContainId(bytes32 _id) public view returns (bool) {
         for (uint256 i = 0; i < proposals.length; i++) {
